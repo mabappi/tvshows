905fc10c47ee4ae635e2162a4eb9fd30b1496ab6
Bappi Ahmed <bappi.ahmed@collenda.com>
2023-03-01 10:04:41 +0100

    And more tests

---
+++ src/AutomatedTests/UnitTests/MazeConsmer/Services/ScrapperDbContextBuilder.cs
@@ -0,0 +1,22 @@
+﻿using MazeConsumer.DbContext;
+using Microsoft.Extensions.Configuration;
+using UnitTests.BaseLibrary;
+
+namespace UnitTests.MazeConsmer.Services;
+
+internal class ScrapperDbContextBuilder : BuilderBase<ScrapperDbContext>
+{
+    private const string DataDir = "data";
+    protected override ScrapperDbContext BuildInternal()
+        => new ScrapperDbContext(GetConfiguration());
+    private IConfiguration GetConfiguration()
+    {
+        if(!Directory.Exists(DataDir))
+            Directory.CreateDirectory(DataDir);
+        var settings = new Dictionary<string, string> { { "DataDirectory", DataDir } };
+
+        return new ConfigurationBuilder()
+            .AddInMemoryCollection(settings)
+            .Build();
+    }
+}
---
+++ src/AutomatedTests/UnitTests/MazeConsmer/Services/ScrapperDbContextTests.cs
@@ -0,0 +1,37 @@
+﻿namespace UnitTests.MazeConsmer.Services
+{
+    [TestClass]
+    public class ScrapperDbContextTests
+    {
+        [TestMethod]
+        public void GetNextScraperData_ShouldReturnScraperDataWithPageNumber()
+        {
+            var dbContext = new ScrapperDbContextBuilder().Build();
+            var scraperData = dbContext.GetNextScraperData();
+            scraperData.Should().NotBeNull();
+            Assert.AreEqual(0, scraperData.RowFetched);
+            Assert.IsTrue(scraperData.PageNumber > 0);
+        }
+
+        [TestMethod]
+        public void Save_ShouldSaveIgnoreDataWithRowsZero()
+        {
+            var dbContext = new ScrapperDbContextBuilder().Build();
+            var scraperData = dbContext.GetNextScraperData();
+            dbContext.Save();
+            dbContext = new ScrapperDbContextBuilder().Build();
+            Assert.IsFalse(dbContext.Scrapers.Any(x => x.RowFetched == 0));
+        }
+
+        [TestMethod]
+        public void Save_ShouldSaveDataWithRowCount()
+        {
+            var dbContext = new ScrapperDbContextBuilder().Build();
+            var scraperData = dbContext.GetNextScraperData();
+            scraperData.RowFetched = 100;
+            dbContext.Save();
+            dbContext = new ScrapperDbContextBuilder().Build();
+            Assert.IsFalse(dbContext.Scrapers.Any(x => x.RowFetched == 0));
+        }
+    }
+}
